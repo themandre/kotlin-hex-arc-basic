efaedea26eb2609f19d6d3ede7cf00f34f242f62
Sergio Herrera Piqueras <sergiohp9@gmail.com>
2023-12-08 13:40:24 +0100

    docs: set springdoc naming strategy to snake_case

--- src/main/kotlin/com/mandre/kha/users/infrastructure/adapters/config/BeanConfiguration.kt
+++ src/main/kotlin/com/mandre/kha/users/infrastructure/adapters/config/BeanConfiguration.kt
@@ -1,8 +1,12 @@
 package com.mandre.kha.users.infrastructure.adapters.config
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.mandre.kha.users.domain.service.UserService
+import io.swagger.v3.core.jackson.ModelResolver
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+
 
 @Configuration
 class BeanConfiguration {
@@ -10,5 +14,10 @@ class BeanConfiguration {
     @Bean
     fun userService(): UserService {
         return UserService()
+    }
+
+    @Bean
+    fun modelResolver(objectMapper: ObjectMapper): ModelResolver {
+        return ModelResolver(objectMapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE))
     }
 }
